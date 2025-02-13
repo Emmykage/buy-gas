7f7349d4a25cc33a5ce1fa906d8946da195ba735
morris menanya <emmiemenz@gmail.com>
2025-02-13 17:42:20 +0100

    cta fix

--- src/components/CTA/CAT.jsx
+++ src/components/CTA/CAT.jsx
@@ -9,7 +9,7 @@ const CTA = ({title, text,  classPro, buttonText , link = "/contact-us"}) => {
         <div className={`py-16 max-w-6xl text-alt  m-auto ${classPro}`}>
           {title &&  <h4 className='text-xl font-medium md:text-3xl text-center'>{title}</h4> }
          
-            <p className='text-center text-xl font-medium text-gray-600'>{text || "MAke an Order for our Gas delivery"}</p>
+            <p className='text-center text-xl font-medium text-gray-600'>{text || "Make an Order for our Gas delivery"}</p>
             <AppButton
             style={{
                 width: "max",
@@ -17,7 +17,7 @@ const CTA = ({title, text,  classPro, buttonText , link = "/contact-us"}) => {
             }}>
             {buttonText}
             </AppButton>
-            <NavLink to={link} className="my-5 font-semibold block m-auto w-max px-10 py-4 border-[3px] rounded-3xl border-alt hover:text-alt">{buttonText || "Contact Us"}</NavLink>
+
 
             
         </div>

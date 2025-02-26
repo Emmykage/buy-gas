4ae39e9bb9b8ff9e6318bdb92daa9d5d40862a1f
morris menanya <emmiemenz@gmail.com>
2025-02-26 07:10:40 +0100

    make nav overlay

--- src/pages/dashboard/components/navigation/Navigation.jsx
+++ src/pages/dashboard/components/navigation/Navigation.jsx
@@ -6,7 +6,7 @@ const Navigation = () => {
     const navigate = useNavigate()
     const [show, setShow] = useState(false)
   return (
-    <header className='py-5 px-4 border-b border-gray-500'>
+    <header className='py-5 px-4 border-b border-gray-500 z-50'>
 
         <div className='flex items-center justify-between'>
             <NavLink to={"/"}>
@@ -14,7 +14,7 @@ const Navigation = () => {
             </NavLink>
 
             <div className='text-alt  font-medium'>
-                <ul className={`${show ? "right-0"  :  "right-full"} md:flex absolute w-full bg-blue-50 md:bg-transparent  top-0 h-screen md:h-auto pt-10 md:pt-0 px-4 md:static transition-all duration-300 ease-in-out gap-4`}>
+                <ul className={`${show ? "right-0"  :  "right-full"} md:flex absolute w-full bg-blue-50 z-50 md:bg-transparent  top-0 h-screen md:h-auto pt-10 md:pt-0 px-4 md:static transition-all duration-300 ease-in-out gap-4`}>
                     <TiThMenuOutline onClick={() => setShow(prev => !prev)} className='text-3xl text-primary md:hidden'/>
 
                     <li className='mt-10 md:mt-0'>

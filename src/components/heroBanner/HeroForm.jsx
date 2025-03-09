934f62401343f025b58027586ff887358215d9b2
morris menanya <emmiemenz@gmail.com>
2025-03-09 06:33:41 +0100

    fix form

--- src/components/heroBanner/HeroForm.jsx
+++ src/components/heroBanner/HeroForm.jsx
@@ -20,8 +20,8 @@ const HeroForm = () => {
   return (
     <div className='bg-gray-0 w-full md:my-0 m-auto heroform px-5 md:px-10 py-10 max-w-3xl text-black text-center text-sm'>
         <p className=' my-10 text-4xl text-theme font-semibold'>Gas <span className='text-primary'>Waka</span> </p>
-        {/* <p className='font-semibold text-gray-700 text-2xl'>Fair Prices, Fast Delivery</p>
-        <p className='font-medium text-gray-700 text-lg'>Get Cooking Gas for 1,300 per Kg Prices, Fast Delivery</p> */}
+        <p className='font-semibold text-gray-700 text-2xl'>Fair Prices, Fast Delivery        </p>
+        <p className='font-medium text-gray-700 text-lg'>Get Cooking Gas for 1,300 per Kg</p>
         <p className='mt-10 font-medium text-lg text-gray-700 my-2'>Enter your details to begin</p>
 
 
